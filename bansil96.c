79f8baf7b04e622577f402becda2b9baabe38f86
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-26 18:10:26 +0630

    first commit

--- bansil96.c
+++ bansil96.c
@@ -1,2 +1,27 @@
+/*
+Q.2 Write a Program to find the length of the String by passing a string as an argument using UDF.For example,
+Input:
+Enter any string: development
+
+Output:
+Length is: 11*/
 #include<stdio.h>
-#include<string.h>
+
+length(){
+
+char a[100];
+
+printf("Enter the string : ");
+scanf("%s",a);
+
+int b=0;
+
+for(int i=0;a[i]!=0;i++){
+b++;
+}
+printf("The length is : %d ",b);
+
+}
+void main(){
+    length();
+}
