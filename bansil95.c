0cad6ba28d99020ac8e0fd2c06ec3d4b5e6f6dd5
bansilghori1 <136827896+bansilghori1@users.noreply.github.com>
2023-07-26 18:11:19 +0630

    first commit

--- bansil95.c
+++ bansil95.c
@@ -1,2 +1,39 @@
+/*Q.1 Write a Program to find the sum of all 1D Array elements by passing an array as an argument using UDF.
+For example,
+Input:
+Enter array size: 5
+Enter array elements:
+a[0] = 6
+a[1] = 4
+a[2] = 1
+a[3] = 5
+a[4] = 2
+
+Output:
+The sum of an Array: 18
+*/
 #include<stdio.h>
-#include<string.h>
+
+void array(){
+
+int i,sum=0;
+
+printf("Enter the size of array : ");
+scanf("%d",&i);
+
+int A[i];
+printf("Enter the elements of array : \n");
+
+for(int j=0;j<i;j++){
+    printf("a[%d]=",j);
+    scanf("%d",&A[j]);
+    sum=sum + A[j];
+}
+
+printf("\nTHE SUM OF ALL ARRAY'S ELEMETS IS : %d",sum);
+} 
+
+void main (){
+
+    array();
+}    
